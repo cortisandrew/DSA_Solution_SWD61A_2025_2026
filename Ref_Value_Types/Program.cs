66c58dd018cc16ac312246eacde399196afd9854
andrewc <andrew.cortis@mcast.edu.mt>
2026-02-09 11:00:27 +0100

    First update

--- Ref_Value_Types/Program.cs
+++ Ref_Value_Types/Program.cs
@@ -1,3 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadKey();
+
+using Ref_Value_Types;
+
+Console.WriteLine("Value Type");
+
+int a = 5;
+int b = 6;
+
+Console.WriteLine($"The value of {nameof(a)}: {a}");    // 5
+Console.WriteLine($"The value of {nameof(b)}: {b}");    // 6
+Console.WriteLine();
+Console.WriteLine();
+
+a = b;
+Console.WriteLine($"The value of {nameof(a)}: {a}");    // 6
+Console.WriteLine($"The value of {nameof(b)}: {b}");    // 6
+Console.WriteLine();
+Console.WriteLine();
+
+b = 10;
+Console.WriteLine($"The value of {nameof(a)}: {a}");    // 6
+Console.WriteLine($"The value of {nameof(b)}: {b}");    // 10
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("Reference Type");
+ReferenceTypeClass refA = new ReferenceTypeClass() { Value = 5 };
+ReferenceTypeClass refB = new ReferenceTypeClass() { Value = 6 };
+
+Console.WriteLine($"The value of {nameof(refA)}: {refA.Value}");    // 5
+Console.WriteLine($"The value of {nameof(refB)}: {refB.Value}");    // 6
+Console.WriteLine();
+Console.WriteLine();
+
+refA = refB;
+Console.WriteLine($"The value of {nameof(refA)}: {refA.Value}");    // 6
+Console.WriteLine($"The value of {nameof(refB)}: {refB.Value}");    // 6
+Console.WriteLine();
+Console.WriteLine();
+
+refB.Value = 10;
+Console.WriteLine($"The value of {nameof(refA)}: {refA.Value}");    // 10
+Console.WriteLine($"The value of {nameof(refB)}: {refB.Value}");    // 10
+Console.WriteLine();
+Console.WriteLine();
+
---
+++ Ref_Value_Types/ReferenceTypeClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ref_Value_Types
+{
+    public class ReferenceTypeClass
+    {
+        public int Value { get; set; }
+    }
+}
