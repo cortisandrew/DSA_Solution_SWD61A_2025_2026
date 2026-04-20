716b4ab030a03f76dbeeb974bf363e806a1cab42
andrewc <andrew.cortis@mcast.edu.mt>
2026-04-20 09:09:44 +0200

    Update

--- CodeExamples/Program.cs
+++ CodeExamples/Program.cs
@@ -1,11 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
 Console.WriteLine("Hello, World!");
 
-int n;
+int n = 10;
 int t;
 
-int[] problemSizes = [3, 4, 5, 6, 7, 8, 9, 10];
+Console.WriteLine(E(n));
+//Console.WriteLine(C(n));
 
+
+//int[] problemSizes = [3, 4, 5, 6, 7, 8, 9, 10];
+
+/*
 foreach (int problemSize in problemSizes)
 {
     int[] X = new int[problemSize];
@@ -19,6 +26,7 @@ foreach (int problemSize in problemSizes)
 
     Console.WriteLine($"n: {n},\t t:{t}"); // 3n + 1
 }
+*/
 
 /*
 _ = FindMax(
@@ -28,7 +36,95 @@ _ = FindMax(
     );
 */
 
+static int A(int n)
+{
+    int count = 0;
 
+    for (int i = 0; i < n; i++)
+    {
+        count++;
+    }
+    return count;
+}
+
+static int B(int n) // Theta(n^2)
+{
+    int count = 0;
+
+    for (int i = 0; i < n; i++) // repeat the inner loop xn
+    {
+        for (int j = 0; j < n; j++) // repeat the count++ xn
+        {
+            count++;                // O(1) work
+        }
+    }
+    return count;
+}
+
+static int C(int n) // Theta(n)
+{
+    int count = 0;
+
+    for (int i = 0; i < n; i++)// n
+    {
+        count++;
+    }
+    for (int i = 0; i < n; i++)// n
+    {
+        count++;
+    }
+    for (int i = 0; i < n; i++)// n
+    {
+        count++;
+    }
+
+    // n+n+n = 3n
+    return count;
+}
+
+// Theta(n^2)
+static int D(int n)
+{
+    int count = 0;
+
+    for (int i = 0; i < n; i++)// n
+    {
+        count++;
+    }
+
+    for (int i = 0; i < n; i++) // repeat the inner loop xn
+    {
+        for (int j = 0; j < n; j++) // repeat the count++ xn
+        {
+            count++;                // O(1) work
+        }
+    }
+
+    // n + n^2
+    return count;
+}
+
+static int FindMaxLinQ(int[] X)
+{
+    return X.Max(x => x);
+}
+
+static int E(int n) // Exercise!!!
+{
+    int count = 0;
+
+    for (int i = 0; i < n; i++) 
+    {
+        int countBefore = count;
+        for (int j = i; j < n; j++) // j starts from i instead of 0
+        {
+            //Console.WriteLine($"{i},{j}");
+            count++;                // O(1) work
+        }
+        Console.WriteLine($"Work for inner look with i={i} is {count - countBefore}");
+    }
+    return count;
+}
 
 
 static int FindMax(int[] X, out int n, out int t)
